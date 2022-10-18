a393cea5127fb0ae1ca3709f093e0a76e6d95602
s10pao <99454468+s10pao@users.noreply.github.com>
2022-10-18 15:53:41 +0800

    Added SH_TT in BUTTON layer

--- users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -384,6 +384,6 @@ U_NP,              U_NP,              KC_APP,            KC_SPC,            KC_T
 
 #define MIRYOKU_ALTERNATIVES_BUTTON \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NU,              U_NU,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
+KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           SH_TT,             SH_TT,             KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 U_NP,              U_NP,              KC_BTN3,           KC_BTN1,           KC_BTN2,           KC_BTN2,           KC_BTN1,           KC_BTN3,           U_NP,              U_NP
